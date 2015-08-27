c5c13d97212f10eb87a3a93ce83e197422846d83
sheikp <sheikp@gmail.com>
2015-08-26 23:28:56 -0700

    initial

---
+++ FOWeb/FOWeb.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FOWeb.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
